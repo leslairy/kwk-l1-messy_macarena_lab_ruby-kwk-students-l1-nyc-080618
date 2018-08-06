f89928f90d9abc76d60630787aedadea2ee89d00
Leyla Alekperova <leslairy@gmail.com>
2018-08-06 19:52:27 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -24,7 +24,7 @@ puts "Step 5: Bring your right hand up and around to the back of your head."
 puts "Step 6: Left arm out in front of you, palm facing down."
 
 # Step 7: FIXME
-puts "Step 7: Place your left hand on your left shoulder."
+puts "Step 7: Bring your right hand up and around to the back of your head."
 
 # Step 8: FIXME
 puts "Step 8: Bring your left hand up and around to the back your head."
